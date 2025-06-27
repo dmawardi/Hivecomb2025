f36b249927bcc083d377d2363f554acc74587512
Dannar Mawardi <dMawardi@gmail.com>
2025-06-27 23:25:11 +1000

    Created About page with basic about header

---
+++ resources/views/about.blade.php
@@ -0,0 +1,12 @@
+<x-layout.app :title="__('About HiveComb - Your Partner in Web Development Excellence')"
+    :description="__('Learn about HiveComb, a web development agency dedicated to delivering innovative and effective solutions for your business. Discover our mission & values.')">
+    <x-honeycomb-float>
+        
+        <x-headers.about></x-headers.about>
+        {{-- <x-our-story></x-our-story>
+        <x-vision-mission></x-vision-mission> --}}
+        <x-sections.process background="bg-violet-950 relative z-40" textColor="text-gray-200"
+            titleTextColor="text-amber-400"></x-sections.process>
+        <x-sections.call-to-action></x-sections.call-to-action>
+    </x-honeycomb-float>
+</x-layout.app>
---
+++ resources/views/components/headers/about.blade.php
@@ -0,0 +1,36 @@
+<section id="introduction" class="bg-violet-950 py-16">
+
+    <div class="container mx-auto px-4 relative z-40">
+        <div class="flex flex-wrap items-center relative z-20">
+            <!-- Visual Area: Could be an image or a video -->
+            <div class="w-full lg:w-1/2 px-4">
+                <div class="relative w-full h-full">
+                    <img src="/images/hive-cave.webp" alt="HiveComb Team"
+                        class="rounded-lg shadow-lg max-w-full h-auto object-cover">
+                    {{-- <div class="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-black to-transparent rounded">
+                    </div> --}}
+                </div>
+
+                <!-- Alternatively, for a video:
+                <video autoplay loop muted class="rounded-lg shadow-lg max-w-full h-auto">
+                    <source src="/path-to-your-video.mp4" type="video/mp4">
+                    Your browser does not support the video tag.
+                </video>
+                -->
+            </div>
+            <!-- Content Area -->
+            <div
+                class="w-full lg:w-1/2 px-4 mb-8 lg:mb-0 bg-gray-900 rounded-md shadow-lg border border-solid border-gray-500 py-8 opacity-90">
+                <h1 class="text-3xl lg:text-4xl font-bold text-amber-400 mb-4">
+                    Empowering Digital Transformations
+                </h1>
+                <p class="text-lg text-amber-200">
+                    At HiveComb, we're not just building websites; we're crafting experiences that elevate brands,
+                    engage audiences, and drive digital innovation forward. With a blend of precision, creativity, and
+                    the latest technology, we turn visions into reality.
+                </p>
+            </div>
+
+        </div>
+    </div>
+    </section>
